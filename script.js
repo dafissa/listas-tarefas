7e547f3220d5173dd463a98359db6a54130e8e0a
santos <rayssa.9258@colegioprogresso.g12.br>
2026-05-20 07:49:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const input = document.getElementById('input')
+const button = document.getElementById('button')
 const display = document.getElementById('display')
 
 function inserirTarefa() {
-    display.innerHTML = input.value
+    let tarefa = input.value
+    display.innerHTML += `
+    <div class="tarefa">
+    ${tarefa}
+        <div class="buttons">
+            <button class="delete-button">deletar</button>
+            <button class="edit-button">editar</button>
+        </div>
+    </div>
+    `
+    input.value = ''
 }
 
 button.addEventListener("click", inserirTarefa)
